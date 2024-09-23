7b915777fb9ef52debda04e5d22f1cc829362024
Sana Saeed <ss.uitech01@gmail.com>
2024-09-24 03:27:23 +0500

    fix(todo-form): change date format

--- src/collections/TodoForm/TodoForm.tsx
+++ src/collections/TodoForm/TodoForm.tsx
@@ -1,16 +1,52 @@
+import { useState } from 'react';
 import { Button, DatePicker, Input, TextArea } from '../../components';
 import { FaPlus } from 'react-icons/fa6';
+import { DatePickerProps } from 'antd';
+
 export const TodoForm = () => {
+  const [taskName, setTaskName] = useState('');
+  const [date, setDate] = useState<Date | null>(null);
+  const [description, setDescription] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(taskName, date, description);
+    //TODO: add to context
+    setTaskName('');
+    setDate(null);
+    setDescription('');
+  };
+
+  const onDateChange: DatePickerProps['onChange'] = (date, dateString) => {
+    if (date) {
+      const dateFormat = date.toDate();
+      setDate(dateFormat);
+    }
+  };
   return (
-    <form className='flex flex-col gap-2'>
+    <form onSubmit={onSubmit} className='flex flex-col gap-2'>
       <div className='flex gap-2'>
-        <Input placeholder='Task Name*' required />
-        <DatePicker placeholder='mm/dd/yyyy' format="MM/DD/YYYY" />
+        <Input
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder='Task Name*'
+          required
+        />
+        <DatePicker
+          onChange={onDateChange}
+          placeholder='mm/dd/yyyy'
+          format='MMM DD, YYYY'
+        />
         <Button icon={<FaPlus />}>Add</Button>
       </div>
 
       <div>
-        <TextArea placeholder='Description (optional)' cols={38} />
+        <TextArea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder='Description (optional)'
+          cols={38}
+        />
       </div>
     </form>
   );
