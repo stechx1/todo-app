56e25a817806d20bd27980ce12ebbe7f1cc85170
Sana Saeed <ss.uitech01@gmail.com>
2024-09-23 23:35:44 +0500

    feat: inter font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="w-1/2 h-1/2 bg-gray-100 rounded-lg shadow-lg flex items-center justify-center font-medium text-xl">
-        Your To-Do app goes here! 🚀
-      </div>
+      <p>Inter font</p>
     </div>
   );
 }
